f582136ffce511bd9e334214a6339a6e30726df3
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2023-02-24 22:07:36 +0100

    upload php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+function isMobileDevice() {
+          return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $_SERVER['HTTP_USER_AGENT']);
+}
+
+if(isMobileDevice()){
+          echo "Welcome Mr. Mobile";
+} else {
+          echo "Welcome Mr. Desktop";
+}
